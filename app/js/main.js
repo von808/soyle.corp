f862cb653bba980a89c60444cf490fee28b8688b
Alexey Sergeev <sergeevalexeyweb@gmail.com>
2023-08-29 04:21:23 +0700

    header-login

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,7 @@
-$(function () {
+const burgerBtn = document.getElementById('burger-btn');
+const burgerMenu = document.getElementById('burger-menu');
 
-    
-
-})
+burgerBtn.onclick = function(){
+  burgerBtn.classList.toggle('active')
+  burgerMenu.classList.toggle('active')
+}
