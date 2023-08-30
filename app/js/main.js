aeda17ee23508276c6d7274e9fe46f3239f90858
Alexey Sergeev <sergeevalexeyweb@gmail.com>
2023-08-30 22:52:03 +0700

    deploy main page

--- app/js/main.js
+++ app/js/main.js
@@ -75,7 +75,6 @@ window.intlTelInput(input, {
   utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js",
 });
 
-// Полифилл для метода forEach для NodeList
 if (window.NodeList && !NodeList.prototype.forEach) {
   NodeList.prototype.forEach = function (callback, thisArg) {
     thisArg = thisArg || window;
@@ -91,13 +90,11 @@ document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
   const dropDownListItems = dropDownList.querySelectorAll('.dropdown__list-item');
   const dropDownInput = dropDownWrapper.querySelector('.dropdown__input-hidden');
 
-  // Клик по кнопке. Открыть/Закрыть select
   dropDownBtn.addEventListener('click', function (e) {
     dropDownList.classList.toggle('dropdown__list--visible');
     this.classList.add('dropdown__button--active');
   });
 
-  // Выбор элемента списка. Запомнить выбранное значение. Закрыть дропдаун
   dropDownListItems.forEach(function (listItem) {
     listItem.addEventListener('click', function (e) {
       e.stopPropagation();
@@ -108,7 +105,6 @@ document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
     });
   });
 
-  // Клик снаружи дропдауна. Закрыть дропдаун
   document.addEventListener('click', function (e) {
     if (e.target !== dropDownBtn) {
       dropDownBtn.classList.remove('dropdown__button--active');
@@ -116,7 +112,6 @@ document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
     }
   });
 
-  // Нажатие на Tab или Escape. Закрыть дропдаун
   document.addEventListener('keydown', function (e) {
     if (e.key === 'Tab' || e.key === 'Escape') {
       dropDownBtn.classList.remove('dropdown__button--active');
